351acfa6bfac9bdfcc04e7004cbfe716e2a923e0
Harish <Harishhari0525@gmail.com>
2023-10-20 09:42:53 +0530

    Update ExampleProvider.kt

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -4,9 +4,9 @@ import com.lagradost.cloudstream3.TvType
 import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.SearchResponse
 
-class ExampleProvider : MainAPI() { // all providers must be an instance of MainAPI
-    override var mainUrl = "https://example.com/" 
-    override var name = "Example provider"
+class ExampleProvider : StreamDady() { // all providers must be an instance of MainAPI
+    override var mainUrl = "https://www.streamdady.com/" 
+    override var name = "StreamDady"
     override val supportedTypes = setOf(TvType.Movie)
 
     override var lang = "en"
@@ -18,4 +18,6 @@ class ExampleProvider : MainAPI() { // all providers must be an instance of Main
     override suspend fun search(query: String): List<SearchResponse> {
         return listOf<SearchResponse>()
     }
+  //  override suspend fun load(url: String): LoadResponse? {
+    //    return super.load(url).apply { this?.type = TvType.NSFW }
 }
