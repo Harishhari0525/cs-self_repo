e56a41710b9348bb36159250eec2bb86e9114e9f
Harish <Harishhari0525@gmail.com>
2023-10-20 11:35:17 +0530

    Update StreamDady.kt

--- StreamDady/src/main/kotlin/com/harish/StreamDady.kt
+++ StreamDady/src/main/kotlin/com/harish/StreamDady.kt
@@ -10,18 +10,20 @@ class StreamDady : MainAPI()
 
     override var mainUrl = "https://www.streamdady.com" 
     override var name = "StreamDady"
-    override val supportedTypes = setOf(TvType.Movie)
-
     override var lang = "en"
+    override val supportedTypes = setOf(
+            TvType.Movie,
+        )
 
     // enable this when your provider has a main page
-    override val mainPage = mainPageOf(
-        "movies/" to "Latest")
+    override val hasMainPage = true
+    //override val mainPage = mainPageOf(
+      //  "movies/" to "Latest",)
 
     // this function gets called when you search for something
-    override suspend fun search(query: String): List<SearchResponse> {
-        return listOf<SearchResponse>()
-    }
-    override suspend fun load(url: String): LoadResponse? {
-        return super.load(url).apply { this?.type = TvType.Movie }
+   // override suspend fun search(query: String): List<SearchResponse> {
+     //   return listOf<SearchResponse>()
+    //}
+    //override suspend fun load(url: String): LoadResponse? {
+      //  return super.load(url).apply { this?.type = TvType.Movie }
 }
