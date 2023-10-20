1bdd3359e21f364ce8631faaa6d8ca2804f70028
harishhari0525 <harishhari0525@gmail.com>
2023-10-20 10:21:37 +0530

    update

--- StreamDady/src/main/kotlin/com/harish/ExamplePlugin.kt
+++ StreamDady/src/main/kotlin/com/harish/ExamplePlugin.kt
@@ -1,11 +1,11 @@
-package com.example
+package com.harish
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class TestPlugin: Plugin() {
+class StreamDady: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(StreamDady())
--- StreamDady/src/main/kotlin/com/harish/ExampleProvider.kt
+++ StreamDady/src/main/kotlin/com/harish/ExampleProvider.kt
@@ -1,23 +1,24 @@
-package com.example
+package com.harish
 
 import com.lagradost.cloudstream3.TvType
 import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.SearchResponse
 
-class ExampleProvider : StreamDady() { // all providers must be an instance of MainAPI
-    override var mainUrl = "https://www.streamdady.com/" 
+class StreamDady : StreamDady() { // all providers must be an instance of MainAPI
+    override var mainUrl = "https://www.streamdady.com" 
     override var name = "StreamDady"
     override val supportedTypes = setOf(TvType.Movie)
 
     override var lang = "en"
 
     // enable this when your provider has a main page
-    override val hasMainPage = true
+    override val mainPage = mainPageOf(
+        "movies/" to "Latest")
 
     // this function gets called when you search for something
     override suspend fun search(query: String): List<SearchResponse> {
         return listOf<SearchResponse>()
     }
-  //  override suspend fun load(url: String): LoadResponse? {
-    //    return super.load(url).apply { this?.type = TvType.NSFW }
+    override suspend fun load(url: String): LoadResponse? {
+        return super.load(url).apply { this?.type = TvType.Movie }
 }
